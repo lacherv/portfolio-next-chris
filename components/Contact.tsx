c894467d828b45745e0c564dddc0af1c351a624f
Christian Awelakoue <awelakoue@gmail.com>
2023-06-18 10:13:57 +0200

    Social Link Section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import SocialLinks from "./SocialLinks";
 
 const Contact = () => {
   return (
     <div className="wrapper">
-      <div className="max-w-5xl mx-auto flex flex-col  items-center gap-4  justify-center">
+      <div className="max-w-5xl mx-auto flex flex-col  items-center gap-4 justify-center">
         <h2 className="text-2xl font-bold">Let us create awesome products</h2>
         <p className="text-lg tracking-wide font-medium text-center">
           I am always open to discussing your projects, imporving your online
@@ -18,6 +18,12 @@ const Contact = () => {
             Say Hello!
           </button>
         </a>
+      </div>
+      <div className="mt-8 flex items-center gap-8 justify-center flex-wrap">
+        <SocialLinks title="Github" link="https://github.com/lacherv" />
+        <SocialLinks title="LinkedIn" link="https://www.linkedin.com/in/christian-awelakoue" />
+        <SocialLinks title="Twitter" link="https://twitter.com/lacherv" />
+        <SocialLinks title="Gitlab" link="https://gitlab.com/lacherv" />
       </div>
     </div>
   );
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -7,15 +7,20 @@ const Skills = () => {
     <div className="wrapper">
       <Title text="Skills" icon={<GiSkills />} />
       <div className="flex gap-4 flex-wrap">
+        <SkillsInput title="HTML5" link="https://www.docker.com/" />
         <SkillsInput title="JavaScript" link="" />
+        <SkillsInput title="TypeScript" link="" />
         <SkillsInput title="TailwindCSS" link="https://tailwindcss.com/" />
         <SkillsInput title="CSS3" link="" />
+        <SkillsInput title="Reactjs" link="https://react.dev/" />
+        <SkillsInput title="Nextjs" link="https://nextjs.org/" />
         <SkillsInput title="Trello" link="" />
         <SkillsInput title="Jira" link="" />
         <SkillsInput title="Python" link="" />
-        <SkillsInput title="Reactjs" link="https://react.dev/" />
-        <SkillsInput title="Nextjs" link="https://nextjs.org/" />
+        <SkillsInput title="VS Code" link="" />
+        <SkillsInput title="Jenkins" link="" />
         <SkillsInput title="Nodejs" link="https://nodejs.org/en" />
+        <SkillsInput title="Bash Shell" link="" />
         <SkillsInput title="Git" link="https://git-scm.com/doc" />
         <SkillsInput title="PHP" link="https://www.php.net/" />
         <SkillsInput title="SQL" link="https://www.mysql.com/" />
